759c01e5f89a46f966e204823f861f674358e45e
jradnan <mohd.yasin085208@gmail.com>
2023-05-11 03:46:59 +0600

    practicetask

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import './App.css'
+
+function App() {
+  const addUser = event =>{
+    event.preventDefault()
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name, email}
+    console.log(user);
+    fetch("http://localhost:5000/users",{
+      method :"POST",
+      headers:{
+        "content-type":"application/json"
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.insertedId){
+        alert("users added")
+        form.reset()
+      }
+    })
+  }
+
+  return (
+    <>
+      <h1>Users Management</h1>
+      <Link to={'/users'}>Users</Link>
+      <form onSubmit={addUser}>
+        <input type="text" name='name' placeholder='name' />
+        <br />
+        <input type="email" name="email" id="" placeholder='email' />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Users.jsx
@@ -0,0 +1,39 @@
+import { Link, useLoaderData } from "react-router-dom";
+
+
+const Users = () => {
+
+    const loadedData = useLoaderData();
+    console.log(loadedData);
+    const deleteUser = id => {
+        if (id) {
+            alert('deleted successful')
+
+            fetch(`http://localhost:5000/users/${id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                })
+        }
+
+
+    }
+    return (
+        <div>
+            <Link to={'/'}>Home</Link>
+            <div>
+                {
+                    loadedData.map(data => <p key={data._id}>name:{data.name} <br></br> email:{data.email}
+                        <button onClick={() => deleteUser(data._id)}>X</button>
+                    </p>
+
+                    )
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
